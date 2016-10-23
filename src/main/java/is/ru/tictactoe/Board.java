0ecba4a1b9d9de3fde627aed57d298ccb7b7b2c9
andrikarel <karelandri@gmail.com>
2016-10-23 15:25:52 +0000

    Added player and borad class java files

---
+++ src/main/java/is/ru/tictactoe/Board.java
@@ -0,0 +1,36 @@
+package TicTacToe;
+
+public class Board {
+	
+	
+	Player playerX = new Player('X');
+	Player playerO = new Player('O');
+	
+	char[][] board = new char[2][2];
+	
+	public void draw()
+	{
+		
+	}
+	
+	public boolean input(char input,int turn)
+	{
+		return false;
+	}
+	
+	public void isFree(char input)
+	{
+		
+	}
+	
+	public Board()
+	{
+		
+	}
+
+	
+	
+	
+
+
+}
