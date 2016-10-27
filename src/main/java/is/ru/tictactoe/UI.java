e392411a2b60a2aff6959a133c57a1022e6be823
Gudjon <gudjonss12@ru.is>
2016-10-27 18:28:50 +0000

    UI.drawBoard implemented, no unit tests for this one!

--- src/main/java/is/ru/tictactoe/UI.java
+++ src/main/java/is/ru/tictactoe/UI.java
@@ -1,16 +1,26 @@
-
+package is.ru.tictactoe;
 
 
 
 public class UI {
 
 	// Draws the current state of the board
-	public void drawBoard () {
-
+	public void drawBoard (Board board) {
+		System.out.println('\n');
+        System.out.println(" " + board[0][0] + " | " + board[0][1] + " | " 
+                + board[0][2]);
+        System.out.println(" ---------");
+        System.out.println(" " + board[1][0] + " | " + board[1][1] + " | "
+                + board[1][2]);
+        System.out.println(" ---------");
+        System.out.println(" " + board[2][0] + " | " + board[2][1] + " | "
+                + board[2][2]);
 	}
 
 	// Reads input from StdIn and sends it forward
 	public void getInput () {
 
+		
 	}
 }
+
