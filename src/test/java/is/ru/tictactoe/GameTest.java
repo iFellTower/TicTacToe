c241e74c3e9d7d01a41402a83d800c6b384564b9
andrikarel <karelandri@gmail.com>
2016-10-27 16:31:11 +0000

    removed empy first test

--- src/test/java/is/ru/tictactoe/GameTest.java
+++ src/test/java/is/ru/tictactoe/GameTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
-
+import java.awt.Point;
 
 
 public class GameTest {
@@ -13,10 +13,6 @@ public class GameTest {
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
 
-	@Test
-	public void firstTest() {
-
-	}
 
 	@Test
 	public void checkValidInput() {
@@ -26,7 +22,7 @@ public class GameTest {
 		assertEquals(true,game.validInput(3));
 		assertEquals(true,game.validInput(9));
 	}
-	
+
 	
 
 
