062aef29581b31a422e6efb0f5684f096da6a91d
andrikarel <karelandri@gmail.com>
2016-10-28 15:10:17 +0000

    added function and test for isDraw PP hlynur

--- src/main/java/is/ru/tictactoe/Application.java
+++ src/main/java/is/ru/tictactoe/Application.java
@@ -5,9 +5,40 @@ import java.awt.Point;
 public class Application {
 
 
+	private Game game;
+	private UI ui;
+
+	public Application(Game game, UI ui) {
+		this.game = game;
+		this.ui = ui;
+
+	}
+
+	public void playGame() {
+
+		while(!game.getWinner()) {
+			int input;
+			do{
+				input = ui.getInput();
+			}while(!game.validInput(input));
+
+			game.makeMove(input);
+		}
+
+	}
+
+
 	public static void main(String[] args) {
 
 		Game game = new Game();
 		UI ui = new UI();
+
+		Application app = new Application(game,ui);
+
+		app.playGame();
+
+
+
+
 	}
 }
