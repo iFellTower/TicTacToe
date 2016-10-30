11bf55afdc1c1eb6c7b343deea0f4d9be62975e2
Hlynur Stefánsson <hlynurs15@ru.is>
2016-10-30 23:16:19 +0000

    travis selenium test

--- src/selenium/java/is/ru/tictactoe/SeleniumTestWrapper.java
+++ src/selenium/java/is/ru/tictactoe/SeleniumTestWrapper.java
@@ -5,25 +5,25 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 //import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
 
 public abstract class SeleniumTestWrapper {
-    static FirefoxDriver driver;
+    static WebDriver driver;
     static String baseUrl;
-    // static String port;
+    static String port;
 
     @BeforeClass
     public static void openBrowser(){
         //driver = new ChromeDriver();
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-/*
+
         port = System.getenv("PORT");
         if (port == null) {
             port = "4567";
-        }*/
-        baseUrl = "https://ifelltowerttt.herokuapp.com/";
+        }
+        baseUrl = "http://localhost:" + port;
     }
 
     @AfterClass
