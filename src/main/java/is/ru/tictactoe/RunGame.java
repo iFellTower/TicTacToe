f9bf0f7d73102c8fb01a914d389974fa3a46931e
gunnarg15@ru.is <gunnarg15@ru.is>
2016-10-30 02:20:00 +0000

    Started changin rungame to be an web application, created 2 post requests, makeMove and changePlayer. Also created the relevant index.html test file

--- src/main/java/is/ru/tictactoe/RunGame.java
+++ src/main/java/is/ru/tictactoe/RunGame.java
@@ -1,12 +1,52 @@
 package is.ru.tictactoe;
 
-public class RunGame {
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
+import org.json.simple.JSONObject;
+
+public class RunGame  implements SparkApplication {
 
     public static void main (String[] args) {
-        Game game = new Game();
+        /*Game game = new Game();
         UI ui = new UI();
         Application app = new Application(game,ui);
 
-        app.runApp();
+        app.runApp();*/
+
+        staticFileLocation("/public");
+        SparkApplication runGame = new RunGame();
+        String port = System.getenv("PORT");
+        if (port != null) {
+            port(Integer.valueOf(port));
+        }
+
+        runGame.init();
+    }
+
+    @Override
+    public void init() {
+        final Game game = new Game();
+
+        post("makeMove", (req, res) -> {
+            game.makeMove(
+                Integer.parseInt(req.queryParams("tile"))
+            );
+
+            char playerSymbol = game.getCurrPlayer().getSymbol();
+
+
+            JSONObject move = new JSONObject();
+            move.put("status", "200");
+            move.put("player", Character.toString(playerSymbol));
+            return move;
+        } );
+
+        post("changePlayer", (req, res) -> {
+            game.switchPlayer();
+            res.status(200);
+            return res;
+        });
+
     }
 }
