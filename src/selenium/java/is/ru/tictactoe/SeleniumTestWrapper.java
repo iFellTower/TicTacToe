47af810f0fe113b6dff55b57258e291782ec8c48
Hlynur Stefánsson <hlynurs15@ru.is>
2016-10-30 21:42:57 +0000

    Add selenium java files

---
+++ src/selenium/java/is/ru/tictactoe/SeleniumTestWrapper.java
@@ -0,0 +1,33 @@
+package is.ru.tictactoe;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public abstract class SeleniumTestWrapper {
+    static FirefoxDriver driver;
+    static String baseUrl;
+    static String port;
+
+    @BeforeClass
+    public static void openBrowser(){
+        //driver = new ChromeDriver();
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        port = System.getenv("PORT");
+        if (port == null) {
+            port = "4567";
+        }
+        baseUrl = "http://localhost:" + port;
+    }
+
+    @AfterClass
+    public static void closeBrowser(){
+        driver.quit();
+    }
+}
---
+++ src/selenium/java/is/ru/tictactoe/TestTicTacToeWeb.java
@@ -0,0 +1,29 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestTicTacToeWeb extends SeleniumTestWrapper {
+
+    @Test
+    public void titleMatches() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.get(baseUrl);
+        Thread.sleep(5000);
+        assertEquals("iFellTower - Tic Tac Toe", driver.getTitle());
+        Thread.sleep(5000);
+    }
+    /*
+    @Test
+    public void assertUpdatingNameChangesSpecificJoke() {
+        driver.get(baseUrl + "/config.html");
+
+        1. Fill in some name (first name, last name)
+        2. Submit form.
+        3. Assert that form notifies of success ("Name set as: ...")
+        4. Navigate to page to get specific Joke
+        5. Enter a specific joke number
+        6. Assert the name is used in the joke.
+
+    }*/
+}
